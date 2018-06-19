182c5fb16ba97a8bf6b6a890808943d50e6a5f13
liyy <1290374862@qq.com>
2018-06-19 23:18:30 +0800

    项目搭建

---
+++ app/src/main/java/easynvr/easy/com/easynvr/Activity/StartActivity.java
@@ -0,0 +1,24 @@
+package easynvr.easy.com.easynvr.Activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import easynvr.easy.com.easynvr.R;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+
+
+
+    }
+
+}
